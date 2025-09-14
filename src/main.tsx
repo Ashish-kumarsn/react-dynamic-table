b2bf5d55f3f101c6626d2b1a848621c875411b58
Ashish kumarsn <kaumatchobey@gmail.com>
2025-09-14 08:22:46 +0530

    Updated configs and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import "./index.css";
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
